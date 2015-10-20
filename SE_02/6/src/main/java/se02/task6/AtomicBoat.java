05bfd33d421c637de14abb1cfe7d3beb3c24a1ff
gleifvo <77hagor777@gmail.com>
2015-10-20 18:05:27 +0300

    Задание 6

---
+++ SE_02/6/src/main/java/se02/task6/AtomicBoat.java
@@ -0,0 +1,28 @@
+package se02.task6;
+
+public class AtomicBoat {
+
+	private boolean inMove = false;
+	
+	private EngineAtomicBoat enigneAtomicBoat = new EngineAtomicBoat();
+
+	public String runAtomicBoat() {
+		this.enigneAtomicBoat.Start();
+		
+		this.inMove = true;
+		
+		return "Boat is moving!!";
+	}
+
+	private class EngineAtomicBoat {
+
+		private boolean enable = false;
+
+		public void Start() {
+
+			this.enable = true;
+		}
+
+	}
+
+}
---
+++ SE_02/6/src/main/java/se02/task6/Main.java
@@ -0,0 +1,13 @@
+package se02.task6;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		AtomicBoat atomicBoat = new AtomicBoat();
+
+		System.out.println(atomicBoat.runAtomicBoat());
+
+	}
+
+}
