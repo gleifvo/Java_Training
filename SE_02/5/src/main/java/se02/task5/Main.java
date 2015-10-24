52a1a1b1e999e55c44c95e18ee96b75acb2dc4a0
gleifvo <77hagor777@gmail.com>
2015-10-24 18:57:53 +0300

    Задание 5

--- SE_02/5/src/main/java/se02/task5/Main.java
+++ SE_02/5/src/main/java/se02/task5/Main.java
@@ -17,24 +17,24 @@ public class Main {
 		studentsGroups.add(new StudentGroup(Discipline.Russian));
 		
 
-		studentsGroups.get(0).addStudent("Иван");
+		studentsGroups.get(0).addStudent("Name");
 		
-		studentsGroups.get(1).addStudent("Иван");
+		studentsGroups.get(1).addStudent("Name");
 		
-		studentsGroups.get(2).addStudent("Иван");
+		studentsGroups.get(2).addStudent("Name");
 		
 		
-		studentsGroups.get(0).addMark("Иван", new Double(10));
-		studentsGroups.get(0).addMark("Иван", new Double(9));
+		studentsGroups.get(0).addMark("Name", new Double(10));
+		studentsGroups.get(0).addMark("Name", new Double(9));
 		
-		studentsGroups.get(1).addMark("Иван", new Double(3));
-		studentsGroups.get(1).addMark("Иван", new Double(2));
+		studentsGroups.get(1).addMark("Name", new Double(3));
+		studentsGroups.get(1).addMark("Name", new Double(2));
 		
-		studentsGroups.get(2).addMark("Иван", new Integer(8));
-		studentsGroups.get(2).addMark("Иван", new Integer(6));
+		studentsGroups.get(2).addMark("Name", new Integer(8));
+		studentsGroups.get(2).addMark("Name", new Integer(6));
 
 
-		System.out.println(StudentGroup.findGroupWithBestMarks(studentsGroups, "Иван"));
+		System.out.println(StudentGroup.findGroupWithBestMarks(studentsGroups, "Name"));
 	
 
 
