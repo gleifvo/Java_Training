fb66eadc4527cf57a929f17eca593a498d325b8c
gleifvo <77hagor777@gmail.com>
2015-11-07 18:06:17 +0300

    Задание 2

---
+++ SE_06/2/src/main/java/se06/task1/Main.java
@@ -0,0 +1,22 @@
+package se06.task1;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		PropertyFileReader propertiFileReader = new PropertyFileReader();
+		
+		propertiFileReader.readFile("Ap1p");
+		
+		propertiFileReader.readFile("App");
+		
+		System.out.println(propertiFileReader.getValue("keyq"));
+		System.out.println(propertiFileReader.getValue("key"));
+		System.out.println(propertiFileReader.getValue("key123"));
+		
+		System.out.println(propertiFileReader.toString());
+		
+	}
+
+}
